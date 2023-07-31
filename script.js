69f14bb3959753f5512356c9c72e9ae6b574b1c2
muhibsiddiqui <140731537+muhibsiddiqui@users.noreply.github.com>
2023-07-31 15:25:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+let runningtotal = 0;
+let buffer = "0";
+let previousoperator;
+
+const screen = document.querySelector('.screen');
+
+
+function buttonclick(value) {
+    if (isNaN(value)) {
+        hsymbol(value);
+    }
+    else {
+        hnumber(value);
+
+    }
+    screen.innerText = buffer;
+}
+
+function hsymbol(symbol) {
+    switch (symbol) {
+        case 'C':
+            buffer = '0';
+            runningtotal = 0;
+            break;
+
+        case '=':
+
+            if (previousoperator === null) {
+                return
+            }
+            foperate(parseInt(buffer));
+            previousoperator = null;
+            buffer = runningtotal;
+            runningtotal = 0;
+            break;
+
+        case '←':
+            if (buffer.length === 1) {
+                buffer = '0';
+
+            }
+            else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        case '+':
+            hmath(symbol);
+            break;
+        case '−':
+            hmath(symbol);
+            break;
+        case '×':
+            hmath(symbol);
+            break;
+        case '÷':
+            hmath(symbol);
+            break;
+    }
+}
+
+function hmath(symbol) {
+    if (buffer === "0") { return; }
+
+    const intbuffer = parseInt(buffer);
+    if (runningtotal === 0) {
+        runningtotal = intbuffer;
+
+    }
+    else {
+        foperate(intbuffer);
+
+    }
+    previousoperator = symbol;
+    buffer = '0';
+}
+
+function foperate(intbuffer) {
+    if (previousoperator === '+') {
+        runningtotal += intbuffer;
+
+    }
+    else if (previousoperator === '−') {
+        runningtotal -= intbuffer;
+
+    }
+    else if (previousoperator === '×') {
+        runningtotal *= intbuffer;
+
+    }
+    else if (previousoperator === '÷') {
+        runningtotal /= intbuffer;
+
+    }
+}
+
+function hnumber(numberString) {
+    if (buffer === "0") {
+        buffer = numberString;
+
+    }
+    else { buffer += numberString; }
+}
+
+function init() {
+    document.querySelector('.calc-buttons').addEventListener('click', function (event) {
+        buttonclick(event.target.innerText);
+    })
+
+}
+
+init();
